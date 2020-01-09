97b63f4d4c5f66e449d834221c709aa58acfd44a
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-01-09 02:35:13 +0100

    added $wordFish Selector

--- app.js
+++ app.js
@@ -2,29 +2,37 @@
 
 // Initialize swordfish
 const http = new SwordFish;
+// Workbench APIs
+const url1 = 'https://jsonplaceholder.typicode.com/posts';
+const url2 = 'https://jsonplaceholder.typicode.com/todos';
+const url3 = 'https://jsonplaceholder.typicode.com/users';
+const url4 = 'https://jsonplaceholder.typicode.com/users/1';
+const url5 = 'https://jsonplaceholder.typicode.com/posts/1';
+const url6 = 'https://reqres.in/api/users/2';
 
 // Swordfish Error Object pattern
 // const errObj = {error: 'string'} - can be returned from methods
 
 // function definitions 
-const handleResponse = (res) => {console.log(res)};
-const progIn = () => {console.log('onprogres fired')};
-const progOut = () => {console.log('onprogres finished')};
+// const handleResponse = (res) => {console.log(res)};
+// const progIn = () => {console.log('onprogres fired')};
+// const progOut = () => {console.log('onprogres finished')};
+// const progToggle = () => {console.log('onprogres toggled')};
 
 const handleRes = (res) => {
     let lis = ''; 
-    res.data.forEach(el => {
-        lis += `<li>${el.first_name}: ${el.email}</li>`
+    res.forEach(el => {
+        lis += `<li>${el.email}</li><hr>`
     });
     let html = `<ul>${lis}</ul>`;
-    document.querySelector('.output').innerHTML = html;
+    $F('.output').innerHTML = html;
 };
 
 // Swordfish methods
 // GET METHODS
 // .get(url, fn) - fetch data from url and handle response with callback - fn;
 // \/  \/  \/
-// http.get('https://jsonplaceholder.typicode.com/posts', handleResponse);
+http.get(url3).then(handleRes);
 
 // .getProgress(url, fn1, fn2, fn3) - fetch data from url and handle onprogress function eg. loader
 // fn1 - response handling cb, fn2 - loader init cb, fn3 - loader hide cb
@@ -38,28 +46,41 @@ const handleRes = (res) => {
 
 
 // POST METHOD
-const newUser = {
-    name: 'Jhon',
-    job: 'swordfish library developer'
-};
-//  .post(url, data, fn) - posts a new resource to a server
+// const newUser = {
+//     name: 'Jhon',
+//     job: 'swordfish library developer'
+// };
+// //  .post(url, data, fn) - posts a new resource to a server
 // data = data object, fn = callback to handle response
 // returns posted object with unique server id
 // http.post('https://reqres.in/api/users/2', newUser, handleResponse);
-const url = 'https://reqres.in/api/users/2';
-const url2 = 'https://jsonplaceholder.typicode.com/posts/1';
+
 
 //  .postStatus(url, data, fn) - posts a new resource to a server
 // data = data object, fn = callback to handle responses
 // returns http status to be handled by cb *** success post status: 201
 // http.postStatus('https://reqres.in/api/users/2', newUser, handleResponse);
 
-http.getSome('https://jsonplaceholder.typicode.com/users', 12)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+// http.getSome('https://jsonplaceholder.typicode.com/users', 12)
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
 
 // http.put(url, newUser, handleResponse);
 
 //  postStatus(); posts resource and returns http status
 // http.putStatus(url, newUser, handleResponse);
-// http.delete(url2, handleResponse);
+// // http.delete(url2, handleResponse);
+//     window.$F = function(selector){
+//     const el = document.querySelector(`${selector}`);
+//     return el
+// }
+
+// $F('.output').innerText = 'dupa';
+// document.querySelector('.output').innerText = 'dupsko';
+
+// window.$F = function(selector){
+//     const el = document.querySelector(`${selector}`);
+//     return el
+//   }
+
+//   $F('.output').innerText = 'dupa';
--- swordfish.js
+++ swordfish.js
@@ -1,158 +1,131 @@
-// *********************************
-// ** Swordfish HTTP Library *******
-// **** version 0.1.0 **************
-// ** barracudadev.com/swordfish/ **
-// ** Barracuda Development ********
-// ** barracudadev.com *************
-// *********************************
-function SwordFish(){
-    this.http = new XMLHttpRequest();
-}
-// Get request method .get(url, handleResponse)
-SwordFish.prototype.getXHR = function(url, callback){
-        this.http.open('GET', url, true);    
-        this.http.onerror = () => {
-            callback({error: `error status: ${this.http.status}`})
-        }
-        this.http.onload = () => {
-            if(this.http.status === 200){
-            callback(JSON.parse(this.http.responseText));
-        } 
+/* * * * * * * * * * * * * * * * * * * * * * * 
+ * * Swordfish HTTP Library
+ * * @version 0.1.0
+ * * @documentation: https://github.com/TomaszPietrzykowski/swordFishHTTP/blob/master/README.md
+ * * @author: Tomasz Pietrzyokowski
+ * * www: tomaszpietrzykowski.com
+ * * * * * * * * * * * * * * * * * * * * * * * 
+**/
+!function(global){
+// Attach $wordFishSelector method to window object:
+    global.$F = function(sel){
+      const el = document.querySelector(`${sel}`);
+      return el
     }
-    this.http.send();
-}
-// Get request method with handling loader .getProgress(url, handleResponse, progIn, progOut)
-SwordFish.prototype.getProgress = function(url, callback, progIn, progOut=progIn){
-        this.http.open('GET', url, true);    
-        this.http.onerror = () => {
-            callback({error: `error status: ${this.http.status}`})
+    // SwordFish constructor
+    class SwordFish {
+        constructor() {
+            this.http = new XMLHttpRequest();
+        }   
+        // GET request with async/await
+        async get(url){
+            const serverResponse = await fetch(url);
+            const response = await serverResponse.json();
+            return response;
         }
-        this.http.onprogress = () => progIn();
-        this.http.onload = () => {
-            if(this.http.status === 200){
-            progOut();
-            callback(JSON.parse(this.http.responseText));
-        } 
-    }
-    this.http.send();
-}
-//  Get raw API data output
-SwordFish.prototype.getRaw = function(url, callback){
-    this.http.open('GET', url, true);    
-    this.http.onerror = () => {callback(`error status: ${this.http.status}`)}
-    this.http.onload = () => {if(this.http.status === 200){callback(this.http.responseText);}}
-    this.http.send();
-}
-//  POST METHODS
-//  postStatus(); posts resource and returns http status
-SwordFish.prototype.post = function(url, data, callback){
-    this.http.open('POST', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
-    this.http.onload = () => {
-        if(this.http.status === 201)
-        {callback(JSON.parse(this.http.responseText))
-        } else {callback({error: `status: ${this.http.status}`})}}
-    this.http.send(JSON.stringify(data));
-};
-//  postStatus(); posts resource and returns http status
-SwordFish.prototype.postStatus = function(url, data, callback){
-    this.http.open('POST', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
-    this.http.onerror = () => callback(this.http.status);
-    this.http.onload = () => callback(this.http.status);   
-    this.http.send(JSON.stringify(data));
-};
+        // GET request with async/await with limit param
+        async getSome(url, limit){
+            const serverResponse = await fetch(url);
+            const response = await serverResponse.json();
+            const data = Array.from(response);
+            const limited = [];
+            for(let i = 0; i < limit; i++){
+                limited.push(data[i]);
+            }
+            return limited;
+        }
 
-// PUT METHODS 
-// .put(); updates the resorce with id passed in url, returns updated object
-SwordFish.prototype.put = function(url, data, callback){
-    this.http.open('PUT', url, true);
-    this.http.onerror = () => {callback({error: `status: ${this.http.status}`});}
-    this.http.setRequestHeader('Content-type', 'application/json');
-    this.http.onload = () => callback(JSON.parse(this.http.responseText));
-    this.http.send(JSON.stringify(data));
-};
-//  patch(); updates resource and returns http status
-SwordFish.prototype.putStatus = function(url, data, callback){
-    this.http.open('PATCH', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
-    this.http.onerror = () => callback(this.http.status);
-    this.http.onload = () => callback(this.http.status);   
-    this.http.send(JSON.stringify(data));
-};
-
-// DELETE METHODS
-// Delete request method .delete(url, cb); id passed in url returns status
-SwordFish.prototype.delete = function(url, callback){
-    this.http.open('DELETE', url, true);    
-    this.http.onerror = () => callback(`error status: ${this.http.status}`);
-    this.http.onload = () => callback(this.http.status);
-    this.http.send();
-}
-// ******************************************************************
-// SwordFish.loader();
-// simple loader with a text output into a span of passed selector
-//  to be used with fields awaiting for data 
-// can be used with .getProgress method as a cb
-// static mathod called on a class SwordFish
-// .loader() takes 4 parameters;
-// to stop the loader call it with two arguments, querySelector and 'clear'
-// !!! idea .loaders()- let it takke an array of qSelectors loop through an alow multiplw outputs
-//   Arry.forEach(obj)
-
-function loader(qS, s, t, l){
-    if(s === "clear"){
-    const clear = (qS)=>{
-        clearInterval(this.draw);
-        document.querySelector(qS).innerText  = '';
-    };
-    clear(qS);
-    }else{
-    let str = '';
-    this.draw = setInterval(()=>{
-        if(str.length >= l){
-            str = '';
-            str += `${s}`;
-        } else {
-            str += `${s}`;
+        // Get request method .get(url, handleResponse)
+        getXHR(url, callback) {
+            this.http.open('GET', url, true);
+            this.http.onerror = () => {
+                callback({ error: `error status: ${this.http.status}` });
+            };
+            this.http.onload = () => {
+                if (this.http.status === 200) {
+                    callback(JSON.parse(this.http.responseText));
                 }
-        document.querySelector(qS).innerText  = str;
-    }, t);
-}}
-let qSelector = '#sfish-loader'; // querySelector of output span, type: string
-let qSelector2 = '.sfl3'; // querySelector of output span, type: string
-let separator = '*'; // drawn element, type: string;
-let delay = 80; // drawing delay in msec, type: nummber
-let length = 30; // length of a loader in string signs, type: number;
+            };
+            this.http.send();
+        }
+        // Get request method with handling loader .getProgress(url, handleResponse, progIn, progOut)
+        getProgress(url, callback, progIn, progOut = progIn) {
+            this.http.open('GET', url, true);
+            this.http.onerror = () => {
+                callback({ error: `error status: ${this.http.status}` });
+            };
+            this.http.onprogress = () => progIn();
+            this.http.onload = () => {
+                if (this.http.status === 200) {
+                    progOut();
+                    callback(JSON.parse(this.http.responseText));
+                }
+            };
+            this.http.send();
+        }
+        //  Get raw API data output
+        getRaw(url, callback) {
+            this.http.open('GET', url, true);
+            this.http.onerror = () => { callback(`error status: ${this.http.status}`); };
+            this.http.onload = () => {
+            if (this.http.status === 200) {
+                callback(this.http.responseText);
+            }
+            };
+            this.http.send();
+        }
+        //  POST METHODS
+        //  postStatus(); posts resource and returns http status
+        post(url, data, callback) {
+            this.http.open('POST', url, true);
+            this.http.setRequestHeader('Content-type', 'application/json');
+            this.http.onload = () => {
+                if (this.http.status === 201) {
+                    callback(JSON.parse(this.http.responseText));
+                }
+                else {
+                    callback({ error: `status: ${this.http.status}` });
+                }
+            };
+            this.http.send(JSON.stringify(data));
+        }
+        //  postStatus(); posts resource and returns http status
+        postStatus(url, data, callback) {
+            this.http.open('POST', url, true);
+            this.http.setRequestHeader('Content-type', 'application/json');
+            this.http.onerror = () => callback(this.http.status);
+            this.http.onload = () => callback(this.http.status);
+            this.http.send(JSON.stringify(data));
+        }
+        // PUT METHODS 
+        // .put(); updates the resorce with id passed in url, returns updated object
+        put(url, data, callback) {
+            this.http.open('PUT', url, true);
+            this.http.onerror = () => { callback({ error: `status: ${this.http.status}` }); };
+            this.http.setRequestHeader('Content-type', 'application/json');
+            this.http.onload = () => callback(JSON.parse(this.http.responseText));
+            this.http.send(JSON.stringify(data));
+        }
+        //  patch(); updates resource and returns http status
+        putStatus(url, data, callback) {
+            this.http.open('PATCH', url, true);
+            this.http.setRequestHeader('Content-type', 'application/json');
+            this.http.onerror = () => callback(this.http.status);
+            this.http.onload = () => callback(this.http.status);
+            this.http.send(JSON.stringify(data));
+        }
+        // DELETE METHODS
+        // Delete request method .delete(url, cb); id passed in url returns status
+        del(url, callback) {
+            this.http.open('DELETE', url, true);
+            this.http.onerror = () => callback(`error status: ${this.http.status}`);
+            this.http.onload = () => callback(this.http.status);
+            this.http.send();
+        }
+    }
 
-// !!! idea .loaders()- let it takke an array of qSelectors loop through an alow multiplw outputs
-//   Arry.forEach(obj)
 
-// function loaders(qSs, s, t, l){
-//     let arr = qSs
-//     if(s === "clear"){
-//     const clear = (arr)=>{
-//         clearInterval(this.draw);
-//         arr.forEach(el=>{
-//         document.querySelector(el).innerText  = '';
-//         }
-//     };
-//     clear(arr);
-//     }else{
-//     let str = '';
-//     this.draw = setInterval(()=>{
-//         if(str.length >= l){
-//             str = '';
-//             str += `${s}`;
-//         } else {
-//             str += `${s}`;
-//                 }
-//         arr.forEach(el=>{
-//         document.querySelector(el).innerText  = str;
-//     })
-//     }, t);
-// }}
-// let qSs = '#sfish-loader'; // array of querySelectors of output spans, type: array of strings
-// let separator = '*'; // drawn element, type: string;
-// let delay = 80; // drawing delay in msec, type: nummber
-// let length = 30; // length of a loader in string signs, type: number;
+global.SwordFish = SwordFish;
+
+}(this);
+// ******************************************************************
